59c891acce9a1563c59d5f7c98cecafc1c653998
z1c0 <wziegler@live.at>
2021-02-07 12:23:20 +0100

    Days 11 & 12

---
+++ Day11/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+// Day 11
+
+Console.WriteLine("START");
+var sw = Stopwatch.StartNew();
+Part1();
+Console.WriteLine($"END (after {sw.Elapsed.TotalSeconds} seconds)");
+
+void Part1()
+{
+	var lastDistance = 0;
+	var maxDistance = 0;
+	(int x, int y) pos = (0, 0);
+	foreach (var d in ReadInput())
+	{
+		switch (d)
+		{
+			case "n":
+				pos.y -= 10;
+				break;
+			case "ne":
+				pos.x += 10;
+				pos.y -= 5;
+				break;
+			case "se":
+				pos.x += 10;
+				pos.y += 5;
+				break;
+			case "s":
+				pos.y += 10;
+				break;
+			case "sw":
+				pos.x -= 10;
+				pos.y += 5;
+				break;
+			case "nw":
+				pos.x -= 10;
+				pos.y -= 5;
+				break;
+			default:
+				throw new InvalidOperationException();
+		}
+		var dx = Math.Abs(pos.x) / 10.0;
+		var dy = Math.Abs(pos.y) / 10.0;
+		var dxy = dx * 0.5; // every x move, moves as half an y as well
+		lastDistance = (int)(dx + (dy - dxy));
+		maxDistance = Math.Max(maxDistance, lastDistance);
+	}
+	System.Console.WriteLine($"position: {pos}; distance : {lastDistance}; max distance: {maxDistance}");
+}
+
+IEnumerable<string> ReadInput()
+{
+	return File.ReadAllText("input.txt").Split(',');
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+// Day 12
+
+Console.WriteLine("START");
+var sw = Stopwatch.StartNew();
+Part1();
+Part2();
+Console.WriteLine($"END (after {sw.Elapsed.TotalSeconds} seconds)");
+
+void Part1()
+{
+	var dict = ReadInput();
+	var zeroProgs = new SortedSet<int>();
+
+	Visit(0, dict, zeroProgs);
+
+	System.Console.WriteLine($"{zeroProgs.Count} programs contain the program 0.");
+}
+
+void Part2()
+{
+	var groupCount = 0;
+	var dict = ReadInput();
+	while (dict.Any())
+	{
+		var zeroProgs = new SortedSet<int>();
+		Visit(dict.First().Key, dict, zeroProgs);
+		foreach (var p in zeroProgs)
+		{
+			dict.Remove(p);
+		}
+		groupCount++;
+	}
+	System.Console.WriteLine($"{groupCount} programs were found.");
+}
+
+void Visit(int prog, Dictionary<int, List<int>> dict, SortedSet<int> zeroProgs)
+{
+	if (!zeroProgs.Contains(prog))
+	{
+		zeroProgs.Add(prog);
+		foreach (var p in dict[prog])
+		{
+			Visit(p, dict, zeroProgs);
+		}
+	}
+}
+
+Dictionary<int, List<int>> ReadInput()
+{
+	var dict = new Dictionary<int, List<int>>();
+	foreach (var line in File.ReadAllLines("input.txt"))
+	{
+		var tokens = line.Split(" <-> ");
+		var p1 = Int32.Parse(tokens[0]);
+		tokens = tokens[1].Split(", ");
+		foreach (var t in tokens)
+		{
+			var p2 = Int32.Parse(t);
+			if (!dict.ContainsKey(p1))
+			{
+				dict[p1] = new List<int>();
+			}
+			if (!dict[p1].Contains(p2))
+			{
+				dict[p1].Add(p2);
+			}
+		}
+	}
+	return dict;
+}
